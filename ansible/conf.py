7faf3ada56e3c3e6fc7a0e245cc1b94d6fd02f41
Eric Anderson <eanderson@avinetworks.com>
2017-03-22 14:16:07 -0700

    added ansible section

--- ansible/conf.py
+++ ansible/conf.py
@@ -30,7 +30,6 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_bootstrap_theme
 
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
